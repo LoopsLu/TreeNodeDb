7dac97de047d676c469c4f4f7454892579c4d619
LoopsLu <beethovenlu@hotmail.com>
2017-02-17 09:46:18 +0800

    Add Node class and Entity Framework packages.

---
+++ src/TreeNodeDb/Models/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TreeNodeDb.Models
+{
+    public class Node
+    {
+        public string NodeId { get; set; }
+        public string Name { get; set; }
+
+        public string ParentId { get; set; }
+        public Node Parent { get; set; }
+
+        public ICollection<Node> Children { get; set; }
+    }
+}
